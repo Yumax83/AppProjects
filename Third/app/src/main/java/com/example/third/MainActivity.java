7dff08837c08703657a521255c5f01170f4ee168
Yumax83 <yumax83@gmail.com>
2025-04-18 18:29:41 +0300

    update 1804

--- Third/app/src/main/java/com/example/third/MainActivity.java
+++ Third/app/src/main/java/com/example/third/MainActivity.java
@@ -5,13 +5,17 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
@@ -21,10 +25,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int currentProgress = 0;
+    private ProgressBar progressBar;
+
+    private EditText editTextKeyWord;
+    private ChipGroup chipGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -150,5 +161,97 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button buttonForProgress = findViewById(R.id.buttonForProgress);
+        buttonForProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressBar pb = findViewById(R.id.progressBar);
+                pb.setVisibility(View.VISIBLE);
+            }
+        });
+
+        progressBar = findViewById(R.id.progressBarHorisontal);
+        Button startProgress = findViewById(R.id.startProgress);
+
+        startProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentProgress = currentProgress + 10;
+                progressBar.setProgress(currentProgress);
+                progressBar.setMax(100);
+            }
+        });
+
+
+//        Chip chip1 = findViewById(R.id.chip);
+//        chip1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Action Completed", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+        editTextKeyWord = findViewById(R.id.editTextKeyword);
+        chipGroup = findViewById(R.id.chipGroup);
+        Button buttonAdd = findViewById(R.id.buttonAdd);
+        Button buttonShow = findViewById(R.id.buttonShow);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewChip();
+            }
+        });
+
+        buttonShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSelection();
+            }
+        });
     }
+
+    private void showSelection() {
+        int count = chipGroup.getChildCount();
+        String s = null;
+        for (int i = 0; i < count; i++) {
+            Chip child = (Chip) chipGroup.getChildAt(i);
+            if (!child.isChecked()) {
+                continue;
+            }
+            if (s == null) {
+                s = child.getText().toString();
+            } else {
+                s += ", " + child.getText().toString();
+            }
+        }
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void addNewChip() {
+        String keyword = editTextKeyWord.getText().toString();
+
+        if (keyword.isEmpty()) {
+            Toast.makeText(this, "Please enter the keyword", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        LayoutInflater inflater = LayoutInflater.from(this);
+        Chip newChip = (Chip) inflater.inflate(R.layout.layout_chip_entry, this.chipGroup, false);
+        newChip.setText(keyword);
+        chipGroup.addView(newChip);
+        this.editTextKeyWord.setText("");
+
+        newChip.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handelChipCloseIconClicked((Chip) v);
+            }
+        });
+    }
+
+    private void handelChipCloseIconClicked(Chip chip) {
+        ChipGroup parent = (ChipGroup) chip.getParent();
+        parent.removeView(chip);
+    }
+
 }
