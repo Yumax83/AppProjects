c7b3623afe19809983566e225081fdf8ee821424
Yumax83 <yumax83@gmail.com>
2025-04-25 17:50:50 +0300

    update 2504 v2

---
+++ GameQuiz/app/src/main/java/com/example/gamequiz/GameLevels.java
@@ -0,0 +1,40 @@
+package com.example.gamequiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.game_levels);
+
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Button buttonBack = findViewById(R.id.button_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        // переход на 1 уровень
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameLevels.this, Level1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
